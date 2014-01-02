2a8b1581155c721035429b76b557067de9b8e892
Euan Torano <euantorano@gmail.com>
2014-01-02 16:09:18 +0000

    Adding English language from the standard MyAlerts distribution
    Signed-off-by: Euan Torano <euantorano@gmail.com>

---
+++ english/admin/myalerts.lang.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Administration language file.
+ *
+ * Contains language string definitions for the back-end of MyAlerts in the English language.
+ *
+ * @package MyAlerts
+ * @author  Euan Torano <euan@euantor.com>
+ * @license http://opensource.org/licenses/mit-license.php MIT license
+ * @version 1.1.0
+ */
+
+$l['myalerts']                       = "MyAlerts";
+$l['myalerts_pluginlibrary_missing'] = "The selected plugin could not be installed because <a href=\"http://mods.mybb.com/view/pluginlibrary\">PluginLibrary</a> is missing.";
+$l['myalerts_class_outdated']        = "It seems the Alerts class is not up to date. Please ensure the /inc/plugins/MyAlerts/ folder is up to date. (MyAlerts version: {1}, MyAlerts Class version: {2})";
+
+$l['setting_group_myalerts']                        = "MyAlerts Settings";
+$l['setting_group_myalerts_desc']                   = "Settings for the MyAlerts plugin";
+$l['setting_myalerts_enabled']                      = "Enable MyAlerts?";
+$l['setting_myalerts_enabled_desc']                 = "This switch can be used to globally disable all MyAlerts features";
+$l['setting_myalerts_perpage']                      = "Alerts per page";
+$l['setting_myalerts_perpage_desc']                 = "How many alerts do you wish to display on the alerts listing page? (default is 10)";
+$l['setting_myalerts_dropdown_limit']               = "Number of alerts to show in dropdown";
+$l['setting_myalerts_dropdown_limit_desc']          = "How many alerts do you wish to display in the global alerts dropdown? (default is 5)";
+$l['setting_myalerts_autorefresh']                  = "MyAlerts page AJAX autorefresh";
+$l['setting_myalerts_autorefresh_desc']             = "How often (in seconds) do you wish the MyAlerts page to refresh the alerts listing via AJAX? (0 for no autorefresh)";
+$l['setting_myalerts_alert_rep']                    = "Alert on reputation?";
+$l['setting_myalerts_alert_rep_desc']               = "Do you wish for users to receive a new alert when somebody gives them a reputation?";
+$l['setting_myalerts_alert_pm']                     = "Alert on Private Message?";
+$l['setting_myalerts_alert_pm_desc']                = "Do you wish for users to receive an alert when they are sent a new Private Message (PM)?";
+$l['setting_myalerts_alert_buddylist']              = "Alert on Buddy List addition?";
+$l['setting_myalerts_alert_buddylist_desc']         = "Do you wish for users to receive an alert when they are added to another user's buddy list?";
+$l['setting_myalerts_alert_quoted']                 = "Alert when quoted in a post?";
+$l['setting_myalerts_alert_quoted_desc']            = "Do you wish for users to receive an alert when they are quoted in a post?";
+$l['setting_myalerts_alert_post_threadauthor']      = "Alert thread authors on reply?";
+$l['setting_myalerts_alert_post_threadauthor_desc'] = "Do you wish for thread authors to receive an alert when somebody responds to their thread?";
+$l['setting_myalerts_default_avatar']               = "Default Avatar URL";
+$l['setting_myalerts_default_avatar_desc']          = "Specify a URL to an image to use as the default avatar in MyAlerts for users without an avatar set.";
+
+
+$l['myalerts_helpsection_name'] = 'User Alerts';
+$l['myalerts_helpsection_desc'] = 'Basic information relating to the user alerts system in place on this site.';
+
+$l['myalerts_help_info']          = 'Basic Information';
+$l['myalerts_help_info_desc']     = 'Basic information about the user alerts system and how it works.';
+$l['myalerts_help_info_document'] = 'The alerts system on this site provides you with a simple way to see what\'s been happening recently around the site by the way of a simple notification.
+<p>
+	There is a simple count of your oustanding unread alerts found within the header of the site. Clicking upon this count will open a dropdown list of your unread alerts from which you can then progress to the alerts listing page if you so desire.
+</p>
+<p>
+	The alerts listing page can be <a href="usercp.php?action=alerts">found here</a> and contains a list of all the alerts you\'ve received, both unread and read. You can also delete old alerts that you don\'t wish to retain from this page.
+</p>';
+
+$l['myalerts_help_alert_types']          = 'Alert Types';
+$l['myalerts_help_alert_types_desc']     = 'Information about the different types of alerts that can be received.';
+$l['myalerts_help_alert_types_document'] = 'There are many types of alerts that you can receive based on different actions performed around this site. These are the current different actions that cause an alert to be received:
+<br /><br />';
+
+$l['myalerts_task_cleanup_ran']      = 'Read alerts over a week old were deleted successfully!';
+$l['myalerts_task_cleanup_error']    = 'Something went wrong while cleaning up the alerts...';
+$l['myalerts_task_cleanup_disabled'] = 'The alerts cleanup task has been disabled via the settings.';
+
+$l['myalerts_task_title']       = 'MyAlerts Cleanup';
+$l['myalerts_task_description'] = 'A task to cleanup old read alerts. THis is required as otherwise the alerts table could swell to massive sizes.';
+
+$l['myalerts_nav_settings']     = 'Alert Types';
+$l['myalerts_nav_force_alerts'] = 'Force Alerts';
+$l['myalerts_nav_prune']        = 'Prune Alerts';
+
+$l['myalerts_admin_perm_can_settings']     = 'Can enable/disable alert types?';
+$l['myalerts_admin_perm_can_force_alerts'] = 'Can force alert settings on users?';
+$l['myalerts_admin_perm_can_prune']        = 'Can prune alerts?';
+
+$l['myalerts_error_invalid_setting_id'] = 'Invalid setting ID';
---
+++ english/myalerts.lang.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Main front-end language file.
+ *
+ * Contains language string definitions for the front-end of MyAlerts in the English language.
+ *
+ * @package MyAlerts
+ * @author  Euan Torano <euan@euantor.com>
+ * @license http://opensource.org/licenses/mit-license.php MIT license
+ * @version 1.1.0
+ */
+
+$l['myalerts'] = 'MyAlerts';
+
+$l['myalerts_page_title']  = 'Recent Alerts';
+$l['myalerts_page_getnew'] = 'Check for new alerts';
+
+$l['myalerts_alerts'] = 'Alerts';
+
+$l['myalerts_settings_page_title'] = 'Alert Settings';
+
+$l['myalerts_online_location_listing'] = 'Viewing Alerts';
+
+$l['myalerts_unread_title'] = 'Unread Alerts';
+$l['myalerts_view_all']     = 'View All Alerts';
+
+$l['myalerts_loading']           = 'Loading...';
+$l['myalerts_no_alerts']         = 'No alerts to display';
+$l['myalerts_no_new_alerts']     = 'You have no new alerts.';
+$l['myalerts_rep']               = '{1} modified <a href="reputation.php?uid={2}">your reputation</a>. ({3})';
+$l['myalerts_pm']                = '{1} sent you a new private message titled "{2}". ({3})';
+$l['myalerts_buddylist']         = '{1} added you to their buddy list. ({2})';
+$l['myalerts_quoted']            = '{1} quoted you in <a href="{2}">{3}</a>. ({4})';
+$l['myalerts_post_threadauthor'] = '{1} replied to your thread "<a href="{2}">{3}</a>". There may be more posts after this. ({4})';
+
+$l['myalerts_setting_rep']               = 'Receive alert for reputation?';
+$l['myalerts_setting_pm']                = 'Receive alert for Private Message (PM)?';
+$l['myalerts_setting_buddylist']         = 'Receive alert when added to buddylist?';
+$l['myalerts_setting_quoted']            = 'Receive alert when quoted in a post?';
+$l['myalerts_setting_post_threadauthor'] = 'Receive alert when somebody replies to your thread?';
+$l['myalerts_settings_save']             = 'Save Settings';
+$l['myalerts_settings_updated']          = 'Alert settings updated successfully. Redirecting back to settings page.';
+$l['myalerts_settings_updated_title']    = 'Alert Settings Updated.';
+
+$l['myalerts_delete_deleted']         = 'Alert deleted successfully.';
+$l['myalerts_delete_error']           = 'Alert cannot be deleted at this time.';
+$l['myalerts_delete_read_confirm']    = 'Really delete all read alerts? This cannot be undone!';
+$l['myalerts_delete_all_confirm']     = 'Really delete all alerts? This cannot be undone!';
+$l['myalerts_delete_mass_deleted']    = 'Alerts Deleted';
+$l['myalerts_delete_mass_error']      = 'Error Deleting Alerts';
+$l['myalerts_delete_mass_error_more'] = 'Sorry, something went wrong while deleting your alerts. Please try again.';
+$l['myalerts_delete_all_read']        = 'All read alerts deleted successfully.';
+$l['myalerts_delete_all']             = 'All alerts deleted successfully.';
+
+$l['myalerts_usercp_nav']             = 'Alerts';
+$l['myalerts_usercp_nav_alerts']      = 'View Alerts';
+$l['myalerts_usercp_nav_settings']    = 'Alert Settings';
+$l['myalerts_usercp_nav_delete_read'] = 'Delete Old Alerts';
+$l['myalerts_usercp_nav_delete_all']  = 'Delete All Alerts';
+
+$l['myalerts_help_alert_types']                   = 'Alert Types';
+$l['myalerts_help_alert_types_rep']               = '<strong>Reputation</strong>
+<p>
+	This alert type is received when somebody modifies your reputation.
+</p>';
+$l['myalerts_help_alert_types_pm']                = '<strong>Private Message</strong>
+<p>
+	This alert type is received when somebody sends you a new Private Message via the site. It will show you the sender and the title, much like the notice at the top of the site.
+</p>';
+$l['myalerts_help_alert_types_buddylist']         = '<strong>Added to Buddies</strong>
+<p>
+	This alert type is received whenever another member of the site adds you to his/her buddy list.
+</p>';
+$l['myalerts_help_alert_types_quoted']            = '<strong>Quoted in a post</strong>
+<p>
+	This alert type is received whenever another member of the site quotes you within a post anywhere on the site.
+</p>';
+$l['myalerts_help_alert_types_post_threadauthor'] = '<strong>New reply to your thread</strong>
+<p>
+	This alert type is received whenever another member of the site replies to one of your threads. This alert is only received for the first new post to the thread rather than being duplicated for each reply.
+</p>';
